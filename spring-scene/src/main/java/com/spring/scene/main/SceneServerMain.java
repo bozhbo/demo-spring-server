50d6c9b0e50f273512d6950a1882bd1fb79c9184
bozhbo <z831102@163.com>
2017-08-11 17:47:48 +0800

    first commit

--- spring-scene/src/main/java/com/spring/scene/main/SceneServerMain.java
+++ spring-scene/src/main/java/com/spring/scene/main/SceneServerMain.java
@@ -1,8 +1,21 @@
 package com.spring.scene.main;
 
+import org.springframework.boot.Banner.Mode;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
 public class SceneServerMain {
+	
+	public static ConfigurableApplicationContext context = null;
 
 	public static void main(String[] args) {
-		
+		SpringApplication springApplication = new SpringApplication(SceneServerMain.class);
+
+		springApplication.setBannerMode(Mode.LOG);
+		springApplication.setLogStartupInfo(true);
+
+		context = springApplication.run(args);
 	}
 }
